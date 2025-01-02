870025e0f07634abf67f0f667f6796f08174b216
DaviGabriel1 <davig41441503@gmail.com>
2025-01-02 06:42:11 -1200

    feat: yup validate

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,32 @@
 import {Request, Response} from "express"
+import * as yup from "yup";
 
 interface ICidade{
     nome:string;
+    estado:string
 }
 
+const bodyValidation: yup.Schema<ICidade> = yup.object({
+    nome: yup.string().required().min(3),
+    estado: yup.string().required(),
+  }).required();
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const create = (req:Request<{},{},ICidade>,res:Response) => { //request está com o body tipado
-    const data: ICidade = req.body;
-    console.log(data.nome)
+export const create = async (req:Request<{},{},ICidade>,res:Response) => { //request está com o body tipado
+    
+    let validatedData: ICidade | undefined = undefined;
+
+    try{
+        validatedData = await bodyValidation.validate(req.body)
+
+    }catch(error){
+        const yupError = error as yup.ValidationError;
+        res.json({
+            error:yupError.message
+        })
+        return;
+    }
+    console.log(validatedData);
+
     res.send("create");
     return;
 }
